35a0cb74c137158fe814339ba51976ce1cad2c1e
Stephan Kleinboelting <skleinbo@thp.uni-koeln.de>
2023-01-29 15:58:55 +0100

    @computed docstring

--- src/ComputedFields.jl
+++ src/ComputedFields.jl
@@ -114,11 +114,32 @@ function define_calculateproperty(T, var::Symbol, expr, all_vars::Vector{Symbol}
     )
 end
 
-function strip(ex)
-    ex isa Symbol && return ex
-    ex.head == :(<:) && return ex.args[1]
-end 
+"""
+    @computed mutable struct [...] end
 
+Automatically recompute fields. 
+
+Fields can be assigned an expression with `=` that is reevaluated when
+one of the variables in that expression is set.
+
+# Example
+
+```jldocs
+julia> @computed mutable struct SinCos
+    x::Float64
+    thesincos::Tuple{Float64,Float64} = sincos(x)
+end
+
+julia> sc = SinCos(0.0)
+SinCos(0.0, (0.0, 1.0))
+
+julia> sc.x = pi/2
+1.5707963267948966
+
+julia> sc.thesincos
+(1.0, 6.123233995736766e-17)
+```
+"""
 macro computed(ex)
     if !ex.args[1]
         throw(ErrorException("struct must be mutable"))
